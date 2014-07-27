94ba5f7376b2c205f9ed78b828218d61327c2a83
Slayker <slayker@o2.pl>
2014-07-27 21:46:45 +0100

    Removed references for libraries for included apps

--- packages/amazon/amazon_kindle/Android.mk
+++ packages/amazon/amazon_kindle/Android.mk
@@ -7,19 +7,6 @@ LOCAL_PATH := $(call my-dir)
  LOCAL_SRC_FILES := $(LOCAL_MODULE).apk
  LOCAL_MODULE_CLASS := APPS
  LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX) 
- LOCAL_REQUIRED_MODULES := libAAX_SDK libNativeLibraryWrapper
  include $(BUILD_PREBUILT)
 
-#########################################
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := libAAX_SDK libNativeLibraryWrapper
-				
-LOCAL_SRC_FILES := libs/$(LOCAL_MODULE).so
-
-LOCAL_MODULE_STEM := $(LOCAL_MODULE)
-LOCAL_MODULE_SUFFIX := $(suffix $(LOCAL_SRC_FILES))
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_TAGS := optional
-include $(BUILD_PREBUILT)
 
--- packages/amazon/amazon_mp3/Android.mk
+++ packages/amazon/amazon_mp3/Android.mk
@@ -6,21 +6,6 @@ LOCAL_PATH := $(call my-dir)
  LOCAL_MODULE := amazon_mp3
  LOCAL_SRC_FILES := $(LOCAL_MODULE).apk
  LOCAL_MODULE_CLASS := APPS
- LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX) 
- LOCAL_REQUIRED_MODULES := libaudio_gb libaudio_hc libaudio_ics libaudio_jb libaudio_jb2 libaudio_kk libprhlpr
-						   
+ LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX) 				   
  include $(BUILD_PREBUILT)
-
-#########################################
-include $(CLEAR_VARS)
-
-LOCAL_MODULE :=  libaudio_gb libaudio_hc libaudio_ics libaudio_jb libaudio_jb2 libaudio_kk libprhlpr
-						  
-LOCAL_SRC_FILES := libs/$(LOCAL_MODULE).so
-
-LOCAL_MODULE_STEM := $(LOCAL_MODULE)
-LOCAL_MODULE_SUFFIX := $(suffix $(LOCAL_SRC_FILES))
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_TAGS := optional
-include $(BUILD_PREBUILT)
 
