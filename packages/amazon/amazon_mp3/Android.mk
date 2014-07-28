9003ad4b3ddf30dfb9d501584f5cabe5db9dc9a8
Slayker <slayker@o2.pl>
2014-07-28 23:11:39 +0100

    Added fixed libraries for Amazon mp3

--- packages/amazon/amazon_mp3/Android.mk
+++ packages/amazon/amazon_mp3/Android.mk
@@ -6,6 +6,75 @@ LOCAL_PATH := $(call my-dir)
  LOCAL_MODULE := amazon_mp3
  LOCAL_SRC_FILES := $(LOCAL_MODULE).apk
  LOCAL_MODULE_CLASS := APPS
- LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX) 				   
+ LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+ LOCAL_REQUIRED_MODULES := \
+										libaudio_gb \
+										libaudio_hc \
+										libaudio_ics \
+										libaudio_jb \
+										libaudio_jb2 \
+										libaudio_kk \
+										libprhlpr
  include $(BUILD_PREBUILT)
+
+ #########################################
+include $(CLEAR_VARS)
+LOCAL_MODULE := libaudio_gb 
+LOCAL_SRC_FILES := libs/$(LOCAL_MODULE).so
+LOCAL_MODULE_STEM := $(LOCAL_MODULE)
+LOCAL_MODULE_SUFFIX := $(suffix $(LOCAL_SRC_FILES))
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_PREBUILT)
+
+#########################################
+include $(CLEAR_VARS)
+LOCAL_MODULE := libaudio_hc 
+LOCAL_SRC_FILES := libs/$(LOCAL_MODULE).so
+LOCAL_MODULE_STEM := $(LOCAL_MODULE)
+LOCAL_MODULE_SUFFIX := $(suffix $(LOCAL_SRC_FILES))
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_PREBUILT)
+
+#########################################
+include $(CLEAR_VARS)
+LOCAL_MODULE := libaudio_ics 
+LOCAL_SRC_FILES := libs/$(LOCAL_MODULE).so
+LOCAL_MODULE_STEM := $(LOCAL_MODULE)
+LOCAL_MODULE_SUFFIX := $(suffix $(LOCAL_SRC_FILES))
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_PREBUILT)
+
+#########################################
+include $(CLEAR_VARS)
+LOCAL_MODULE := libaudio_jb2 
+LOCAL_SRC_FILES := libs/$(LOCAL_MODULE).so
+LOCAL_MODULE_STEM := $(LOCAL_MODULE)
+LOCAL_MODULE_SUFFIX := $(suffix $(LOCAL_SRC_FILES))
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_PREBUILT)
+
+#########################################
+include $(CLEAR_VARS)
+LOCAL_MODULE := libaudio_kk 
+LOCAL_SRC_FILES := libs/$(LOCAL_MODULE).so
+LOCAL_MODULE_STEM := $(LOCAL_MODULE)
+LOCAL_MODULE_SUFFIX := $(suffix $(LOCAL_SRC_FILES))
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_PREBUILT)
+
+#########################################
+include $(CLEAR_VARS)
+LOCAL_MODULE := libprhlpr 
+LOCAL_SRC_FILES := libs/$(LOCAL_MODULE).so
+LOCAL_MODULE_STEM := $(LOCAL_MODULE)
+LOCAL_MODULE_SUFFIX := $(suffix $(LOCAL_SRC_FILES))
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_PREBUILT)
+
 
