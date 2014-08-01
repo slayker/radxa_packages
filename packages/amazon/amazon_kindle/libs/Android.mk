638e6f41f79825809d083ce3e9ccd64902d4dd83
Slayker <slayker@o2.pl>
2014-08-01 23:07:00 +0100

    Removed amazon apps

--- packages/amazon/amazon_kindle/libs/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
- 
-include $(CLEAR_VARS)
-LOCAL_PREBUILT_LIBS :=  libAAX_SDK.so \
-										libNativeLibraryWrapper.so
-LOCAL_MODULE_TAGS := optional
-include $(BUILD_MULTI_PREBUILT)
-
-
