40b782ba43cc9d5c1a6805e12a76c3f7aec4988c
Slayker <slayker@o2.pl>
2014-10-23 23:02:23 +0100

    Revert "Adding Xanh apps"
    This reverts commit 4742b2fb051b6d8b6d0306139759bfdad7922172.

--- packages/xanh/es/lib/Android.mk
+++
@@ -1,6 +0,0 @@
-# ES File Manager
-include $(CLEAR_VARS)
-LOCAL_PREBUILT_LIBS := libmyaes.so \
-                       libsapi_so_1.so
-LOCAL_MODULE_TAGS := optional
-include $(BUILD_MULTI_PREBUILT)
--- packages/xanh/spotify/lib/Android.mk
+++
@@ -1,5 +0,0 @@
-# ES File Manager
-include $(CLEAR_VARS)
-LOCAL_PREBUILT_LIBS := liborbit-jni-spotify.so
-LOCAL_MODULE_TAGS := optional
-include $(BUILD_MULTI_PREBUILT)
